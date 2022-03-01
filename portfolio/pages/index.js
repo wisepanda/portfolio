bfa8a856c4221136da5bb0aa786cb699a74f4d62
wisepanda <93228256+wisepanda@users.noreply.github.com>
2022-03-01 20:41:44 +0000

    some styling

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
+import { AiFillLinkedin } from "react-icons/ai"
+import { AiFillGithub } from "react-icons/ai"
+
 
 export default function Home() {
   return (
@@ -12,26 +15,35 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to Ivan's Portfolio
-        </h1> 
+        </h1>
+      <div className={styles.infos}>
+        <div>
+        <Image className={styles.img} src="/untitled1.png" alt='me' width={360} height={360}/>
+        </div>
+        <div className={styles.description}>
+            <p>
+              Ciao sono Ivan!
+            </p>
+        </div>
+      </div>
         <div className={styles.grid}>
         <Link href="/projects">
           <a className={styles.card}>
-            <h2>My Projects</h2>
+            <h4>My Projects</h4>
           </a>
         </Link>
-        <Link href="/about">
+        <Link href="/get-in-touch">
           <a className={styles.card}>
-            <h2>About me..</h2>
-            <p></p>
+            <h4>Get in Touch!</h4>
           </a>
         </Link>
         </div>
         <div>
         {/* <hr/> */}
       <footer className={styles.footer}>
-          <div className={styles.logo}>
-          <button className="linkedin">Linkedin</button>
-          <button className='Github'>Github</button>
+          <div className={styles.logos}>
+            <AiFillLinkedin className={styles.linkedin}/>
+            <AiFillGithub className={styles.github}/>
           </div>
       </footer>
       </div>
