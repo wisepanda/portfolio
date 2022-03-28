ca69f7eef40f7fa0150148383f221be1ac8a3e1c
wisepanda <93228256+wisepanda@users.noreply.github.com>
2022-03-28 14:38:10 +0100

    working on projects page

---
+++ portfolio/pages/projects.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+import EmblaCarousel from "../components/carousel.js"
+
+const SLIDE_COUNT = 5;
+const slides = Array.from(Array(SLIDE_COUNT).keys());
+
+const App = () => (
+  <main>
+    <EmblaCarousel slides={slides} />
+  </main>
+);
+
+export default App
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
